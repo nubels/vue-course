1008f5628376f8b9ed3d5df57dcdca22d35f7ec3
niwortma <nwortmann@icloud.com>
2022-04-16 17:12:50 +0200

    feat: add special attack, heal and surender

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -2,19 +2,51 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
+      playerMana: 100,
       monsterHealth: 100,
       gameOver: false,
     };
   },
+  computed: {},
   methods: {
-    singleTurn() {
+    surrender() {
+      this.playerHealth = 0;
+      this.gameOver = true;
+    },
+    normalTurn() {
       if (!this.gameOver) {
         this.playerAttack();
         this.monsterAttack();
       }
     },
+    specialTurn() {
+      if (this.gameOver) {
+        return;
+      }
+
+      if (this.playerMana <= 0) {
+        console.log("Not enough mana");
+        this.monsterAttack();
+        return;
+      }
+
+      this.playerSpecialAttack();
+      this.monsterAttack();
+    },
+    healTurn() {
+      if (this.gameOver) {
+        return;
+      }
+
+      if (this.playerHealth >= 100 || this.playerMana <= 0) {
+        this.monsterAttack();
+        return;
+      }
+
+      this.playerHeal();
+      this.monsterAttack();
+    },
     playerAttack() {
-      // random betweeen 5 and 10
       const damage = this.generateRandomNumber(5, 10);
       console.log(`Player hits Monster for ${damage}`);
       this.monsterHealth -= damage;
@@ -26,8 +58,28 @@ const app = Vue.createApp({
         console.log("Player wins!");
       }
     },
+    playerHeal() {
+      const heal = this.generateRandomNumber(15, 20);
+      console.log(`Player heals for ${heal}`);
+      this.playerHealth += heal;
+
+      console.log(`Player has ${this.playerHealth} health left`);
+    },
+    playerSpecialAttack() {
+      const damage = this.generateRandomNumber(10, 20);
+      console.log(`Player hits Monster for ${damage}`);
+      this.playerMana -= 50;
+      this.monsterHealth -= damage;
+      console.log(`Monster has ${this.monsterHealth} health left`);
+
+      if (this.monsterHealth <= 0) {
+        this.monsterHealth = 0;
+        this.gameOver = true;
+        console.log("Player wins!");
+      }
+    },
     monsterAttack() {
-      const damage = this.generateRandomNumber(8, 15);
+      const damage = this.generateRandomNumber(6, 12);
       console.log(`Monster hits Player for ${damage}`);
       this.playerHealth -= damage;
 
@@ -37,7 +89,6 @@ const app = Vue.createApp({
         console.log("Monster wins!");
       }
     },
-
     generateRandomNumber(min, max) {
       return Math.floor(Math.random() * (max - min) + min);
     },
